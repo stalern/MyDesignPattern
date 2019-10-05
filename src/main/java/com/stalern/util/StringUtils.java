c781f0a0fe2e120852370329b7ea0df2babc9708
stalern <stalern2000@163.com>
2019-10-05 20:05:56 +0800

    增加了String工具类

---
+++ src/main/java/com/stalern/util/StringUtils.java
@@ -0,0 +1,25 @@
+package com.stalern.util;
+
+/**
+ * 对字符串的工具类
+ * @author stalern
+ * @date 2019/10/5--19:36
+ */
+public class StringUtils {
+    private StringUtils(){}
+    public static boolean isBlank(String str) {
+        int strLen = length(str);
+        if (strLen == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (!Character.isWhitespace(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int length(String str) {
+        return str == null ? 0 : str.length();
+    }
+}
